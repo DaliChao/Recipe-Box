23f1deebe7ce0f528fb11f90d759735e4c7cb9cb
Dali Chao <jasonchao1992@gmail.com>
2018-08-07 23:03:06 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />,
+ReactDOM.render(
+  <div>
+    <div className="top-bar-new">
+          <div className="container">Recipe Box</div>
+        </div>
+    <App />,
+  </div>
+  
   document.getElementById('root')
 );
 registerServiceWorker();
